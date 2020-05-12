2183a57c6fb2344fe62ea7dbf454bb750baf8369
jmsistema <58429706+jmsistema@users.noreply.github.com>
2020-05-12 08:45:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const io = require('socket.io');
 const events = require('./events');
-let port = process.env.PORT || 8081;
+let porta = process.env.PORT || 8081;
+app.listen(porta);
 
 class Server {
   constructor() {
