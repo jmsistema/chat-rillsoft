af2010a82ef91a0eb8fb6c30de72330ec978b6af
jmsistema <58429706+jmsistema@users.noreply.github.com>
2020-05-12 09:10:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,13 @@
 const express = require('express');
 const io = require('socket.io');
 const events = require('./events');
-let port = process.env.PORT || 8081;
 
 
 class Server {
   constructor() {
     this.port = 8081;
     this.app = express();
-    this.server = this.app.listen(this.port, () => {
+    this.server = this.app.listen(process.env.PORT || 8081, () => {
       console.log(`server running http://localhost:${this.port}`);
     });
     this.io = io(this.server);
